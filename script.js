f24466d387215d953828f2fcdea14ebf4ea5599d
LittleBlue418 <hollyathomas88@gmail.com>
2019-11-08 14:21:27 +0100

    parse response text into an object

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     document.getElementById("data").innerHTML = this.responseText;
+    console.log(JSON.parse(this.responseText))
   }
 };
 
